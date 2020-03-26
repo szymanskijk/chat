a85409183b22cf4f2ede36a1c125e266083d9d60
Kuba <szymanski.jk1@gmail.com>
2020-03-26 19:00:24 +0100

    created js file, imported stomp.js library, created websocketconfig

---
+++ src/main/java/jk/szymanski/chat/ChatMessageController.java
@@ -0,0 +1,15 @@
+package jk.szymanski.chat;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatMessageController {
+
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public ChatMessage get(ChatMessage chatMessage){
+        return new ChatMessage(chatMessage.getMessage());
+    }
+}
